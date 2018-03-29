b72db3696aee336d589c399b68feafb9c295fcde
qinjia.zhu <qjzhu@quant-chi.com>
2018-03-29 17:07:58 +0800

    change interface.py

--- interface.py
+++ interface.py
@@ -12,14 +12,13 @@ def interface(sent, flag='zh'):
     :param flag: language flag, 'zh'(Chinese) or 'en'(English)
     :return: risk_level:'高', '中', '低', '无'
     """
-    sent = clean_cut_sent(sent)
-    risk_level = '无'
+
     if flag == 'zh':
         # if input_filter(sent):
         #     pass
         # else:
         #     return '无'
-        label = train_predict(sent)  # negative, neutral
+        label = train_predict(clean_cut_sent(sent))  # negative, neutral
         risk_level = chinese_risk_level(sent, label)
     elif flag == 'en':
         risk_level = english_risk_level(sent)
@@ -30,19 +29,21 @@ def interface(sent, flag='zh'):
 
 if __name__ == '__main__':
 
-    HOST = '192.168.1.251'
-    PORT = 27017
+    # HOST = '192.168.1.251'
+    # PORT = 27017
+    #
+    # conn = MongoClient(host=HOST, port=PORT)
+    # db = conn.TianJi
+    # cnt = 1
+    # with open('record/record_sent.txt', 'a') as fo:
+    #     for i in db.hzgz_tianyu1.find({'events.eventTime.mention':{'$gte':'2017-12-09 00:00:00'},'crawOpt.siteType':{"$regex":"官方网站"}}):
+    #         attn_value = []
+    #         pre_value = []
+    #         result = interface(i['title'])
+    #         print(cnt, clean_cut_sent(i['title']), result)
+    #         fo.write(clean_cut_sent(i['title']) + '\n')
+    #         cnt += 1
+    print(interface('上海抽查：迪士尼宝宝、格林博士等6批次睡袋不合格'))
 
-    conn = MongoClient(host=HOST, port=PORT)
-    db = conn.TianJi
-    cnt = 1
-    with open('record/record_sent.txt', 'a') as fo:
-        for i in db.hzgz_tianyu1.find({'events.eventTime.mention':{'$gte':'2017-12-09 00:00:00'},'crawOpt.siteType':{"$regex":"官方网站"}}):
-            attn_value = []
-            pre_value = []
-            result = interface(i['title'])
-            print(cnt, clean_cut_sent(i['title']), result)
-            fo.write(clean_cut_sent(i['title']) + '\n')
-            cnt += 1
 
 
